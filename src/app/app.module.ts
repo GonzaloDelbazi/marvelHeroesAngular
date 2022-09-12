42ad4e9dead43b7c0ef612f66a609ee708dff9f3
GonzaloDelbazi <gonzalodelbazi@gmail.com>
2022-09-11 22:57:30 -0300

    Creado proyecto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SidebarModule } from 'ng-sidebar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SidebarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+
+
+  opened:boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  _toggleSidebar() {
+    this.opened = !this.opened;
+  }
+
+}
